343326bc068ea7b42c157864e6439a08b70855b5
Wenjie Yang <wenjie.yang@ihandysoft.com>
2017-06-26 16:49:59 +0800

    delete bg

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,1 +1,1 @@
-scp -r /Users/yangwenjie/Desktop/code/webpack/server/route.js root@47.92.70.227:/root/todo/webpack/server/
+scp -r /Users/yangwenjie/Desktop/code/webpack/server/ root@47.92.70.227:/root/todo/webpack/
